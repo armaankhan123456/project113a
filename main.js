8b35be7f38f1e032e3bb044c1d35f1e492f2f1fb
armaankhan123456 <95131439+armaankhan123456@users.noreply.github.com>
2022-06-12 13:35:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Draw a circle at location (30, 30) with a diameter of 20.
+circle(30, 30, 20);
+describe('white circle with black outline in mid of gray canvas');
+
+ellipse(56, 46, 55, 55);
+describe('white ellipse with black outline in middle of a gray canvas');
+
+// Draw a rectangle at location (30, 20) with a width and height of 55.
+rect(30, 20, 55, 55);
+describe('white rect with black outline in mid-right of canvas');
